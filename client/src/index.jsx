2210c0cc1808e41fb66b2b631d7b251e0e072df2
Hack Reactor Student <student@hackreactor.com>
2017-11-13 22:40:05 -0800

    Original commit. Making progress on starter branch

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
+import Movie from './components/movie.jsx';
 
+var movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
 
 class MovieList extends React.Component {
   constructor() {
@@ -9,9 +17,15 @@ class MovieList extends React.Component {
 
   render() {
     return (
-      <div></div>
+      <div>
+        {console.log(movies)}
+        {movies.map( (movie, index) => {
+          return <Movie movie={movie} key={index} />
+        })}
+      </div>
     )
   }
 }
 
 ReactDOM.render( <MovieList />, document.getElementById('app'));
+// TODO : kill it
