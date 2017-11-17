33c7e119ee1cf4eeaf855960d678faebf0f702f9
Patrick Miner <patrickminer318@gmail.com>
2017-11-16 18:30:14 -0800

    Got server talking with helper API in movieAPI.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,8 @@ const movies = [{title: 'Mean Girls', watched: 'false'},
         {title: 'Hackers', watched: 'false'},
         {title: 'The Grey', watched: 'false'},
         {title: 'Sunshine', watched: 'false'},
-        {title: 'Ex Machina', watched: 'false'},]
+        {title: 'Ex Machina', watched: 'false'}];
+const movieAPI = require('./../lib/movieAPI.js');
 
 
 app.use(bodyParser.json());
@@ -15,6 +16,7 @@ app.listen(3000, function () { console.log('MovieList app listening on port 3000
 
 // use app.get to the end path to send movies in the response object
 app.get('/movies', function(req, res) {
+  console.log(movieAPI.hi());
   res.send(movies);
 });
 
